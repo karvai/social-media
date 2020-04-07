e78e372b7e9872863df160f997efd0ee32275011
Ihor <karvai.kip@gmail.com>
2020-04-07 15:08:05 +0100

    Added wireframe for Messages

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import s from "./Dialogs.module.css";
 
 const Dialogs = () => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + " " + s.active}>dimma</div>
+        <div className={s.dialog}>oleg</div>
+        <div className={s.dialog}>igor</div>
+        <div className={s.dialog}>ana</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>How are u?</div>
+        <div className={s.message}>Yo</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
