52c3f0dab62113478a26e0322cc98093a04b86ec
Ihor <karvai.kip@gmail.com>
2020-04-10 18:50:19 +0100

    Importex axios and get users from server

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import style from "./Users.module.css";
+import * as axios from "axios";
+import userPhotoNotSet from "../../assets/img/user.png"
 
 const Users = (props) => {
+
+	axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response =>
+		props.setUsers(response.data.items));
+
 	return (
 		<div>
 			{
 				props.users.map(u => <div key={u.id}>
 					<div className={style.avatar}>
-						<img src={u.photoURL} alt="avatar" />
+						<img src={u.photos.small === null ? userPhotoNotSet : u.photos.small} alt="avatar" />
 					</div>
 					<div>
-						{u.fullName}
+						{u.name}
 					</div>
 					<div>
 						{u.status}
 					</div>
 					<div>
-						{u.location.city}
+						{/* {u.location.city} */}
 					</div>
 					<div>
 						<button onClick={() => { props.toggleFollow(u.id) }}> {u.followed ? "Unfollow" : "Follow"} </button>
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -3,18 +3,18 @@ const SET_USERS = "SET_USERS"
 
 let initialState = {
 	users: [
-		{
-			id: 1, photoURL: 'https://upload.wikimedia.org/wikipedia/commons/1/1b/Facebook_icon.svg',
-			followed: false, fullName: "Igor K", status: "I'm good man", location: { city: "London" }, country: "UK"
-		},
-		{
-			id: 2, photoURL: 'https://pm1.narvii.com/6223/c7d64bf600b12b66226c94863838fc57531244f0_00.jpg',
-			followed: true, fullName: "Pipa V", status: "I'm good man, me too", location: { city: "London" }, country: "UK"
-		},
-		{
-			id: 3, photoURL: "https://pm1.narvii.com/6223/c7d64bf600b12b66226c94863838fc57531244f0_00.jpg",
-			followed: false, fullName: "Kuka P", status: "I'm good man, lol", location: { city: "New York" }, country: "USA"
-		},
+		// {
+		// 	id: 1, photoURL: 'https://upload.wikimedia.org/wikipedia/commons/1/1b/Facebook_icon.svg',
+		// 	followed: false, fullName: "Igor K", status: "I'm good man", location: { city: "London" }, country: "UK"
+		// },
+		// {
+		// 	id: 2, photoURL: 'https://pm1.narvii.com/6223/c7d64bf600b12b66226c94863838fc57531244f0_00.jpg',
+		// 	followed: true, fullName: "Pipa V", status: "I'm good man, me too", location: { city: "London" }, country: "UK"
+		// },
+		// {
+		// 	id: 3, photoURL: "https://pm1.narvii.com/6223/c7d64bf600b12b66226c94863838fc57531244f0_00.jpg",
+		// 	followed: false, fullName: "Kuka P", status: "I'm good man, lol", location: { city: "New York" }, country: "USA"
+		// },
 	],
 };
 
